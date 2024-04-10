6f994a5a62ebf9310975e0153a229fa922c1df88
Dipesh Shrestha <141150408+MrDipuShrestha@users.noreply.github.com>
2024-04-10 07:24:55 +0545

    admin donation uodate

--- admin/index.php
+++ admin/index.php
@@ -327,31 +327,70 @@ if($rows -> num_rows > 0) {
         </div>
     </section>
 
-    <section class="content" id="donation-section" style="display: none;">
+    <section class="content" id="donation-section" style="display:none;">
         <div class="container-fluid">
-            <h2>Donation history</h2>
+            <h2>Donation</h2>
             <table class="table table-secondary table-bordered border-light text-center">
+                <?php 
+                require_once "backend/connect.php";
+$sql = "select * from donation where status = 'pending...'";
+$rows = $conn -> query($sql);
+    ?>
+
                 <thead class="bg bg-info ">
                     <tr>
                         <th>Id</th>
                         <th>Name</th>
-                        <th>Age</th>
-                        <th>Blood Group</th>
-                        <th>Contact No.</th>
+                        <th>Phone</th>
+                        <th>Email</th>
+                        <th>Address</th>
+                        <th>Blood unit</th>
+                        <th>Blood group</th>
+                        <th>Gender</th>
+                        <th>Disease</th>
                         <th>Action</th>
+                </thead>
                 <tbody id="donationTableBody">
+                    <?php
+                    if($rows -> num_rows > 0) {
+                $i = 0;
+    while($data = mysqli_fetch_array($rows)) {
+        ?>
                     <tr>
-                        <td>hii</td>
-                        <td>hii</td>
-                        <td>hii</td>
-                        <td>hii</td>
-                        <td>hii</td>
-                        <td>hii</td>
+                        <td><?php echo ++$i;?></td>
+                        <td><?php echo $data['name']; ?></td>
+                        <td><?php echo $data['phone'] ;?></td>
+                        <td><?php echo $data['email'] ;?></td>
+                        <td><?php echo $data['address'] ;?></td>
+                        <td><?php echo $data['bloodunit'] . ' Unit' ;?></td>
+                        <td><?php echo $data['bloodgroup'] ;?></td>
+                        <td><?php echo $data['gender'];?>
+                        </td>
+                        <td><?php echo $data['disease'] ;?></td>
+                        <td><a href="backend/acceptdonation.php?id=<?php echo $data['id']; ?>"> <button
+                                    class="btn btn-success">Accept</button></a>
+                            <a href="backend/rejectdonation.php?id=<?php echo $data['id'];?>"><button
+                                    class="btn btn-danger">Reject</button></a>
+                        </td>
+                        <!-- <div id="confirmationPopup">
+                            <div id="confirmationBox">
+                                <h2>Are you sure you want to delete?</h2>
+                                <button id="confirmDeleteButton">Yes</button>
+                                <button id="cancelDeleteButton">No</button>
+                            </div>
+                        </div> -->
                     </tr>
+                    <?php  } ?>
+                    <?php
+
+}
+?>
                 </tbody>
             </table>
             </table>
         </div>
+
+
     </section>
 
     <section class="content" id="request-section" style="display: none;">
